0a32760ff3b2bc51f4af888476aaf809d76280cf
Cody Brewer <CB535114@ohio.edu>
2019-03-25 12:23:34 -0400

    Added Whole Project
    Can't seem to get the sprites to work with particle effects correctly

---
+++ 3401_Week10_Brewer/3401_Week10_Brewer/Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    public float delay = 3f;
+    public GameObject explosionEffect;
+    public GameObject smokeEffect;
+    float countdown;
+    bool hasExploded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && ! hasExploded )
+        {
+            Explode();
+           
+            hasExploded = true;
+            
+           
+            
+        }
+        void Explode()
+        {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(smokeEffect);
+            
+        }
+        
+    }
+}
